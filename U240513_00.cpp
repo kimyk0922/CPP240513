0e3a0117dc22ba6b3427c676da0b440ca23530c0
kimyk0922 <kimyk0922@naver.com>
2024-05-13 09:25:27 +0900

    plus_nanana

---
+++ U240513_00.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+
+
+	int Xnumber = 3;
+	int Ynumber = 9;
+	int Znumber = 1;
+
+
+
+		cout << Xnumber + Ynumber <<endl;
+		cout << Ynumber - Znumber <<endl;
+
+		//cout << "31 + 70 = 101";
+
+
+
+
+}
